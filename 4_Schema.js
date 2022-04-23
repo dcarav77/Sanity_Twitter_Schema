7964a0edf5415c11b14c73ba575a3992b65878c6
dcarav77 <57645869+dcarav77@users.noreply.github.com>
2022-04-23 08:22:28 -0400

    Create 4_Schema.js

---
+++ 4_Schema.js
@@ -0,0 +1,10 @@
+import createSchema from 'part:@sanity/base/schema-creator'
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import { userSchema } from '/Users/dcarav77/twitterblockchainapp/twitter-clone-blockchain/thirdtimesacharm/schemas/user_Schema.js'
+import { tweetSchema } from '/Users/dcarav77/twitterblockchainapp/twitter-clone-blockchain/thirdtimesacharm/schemas/Tweet_Schema.js'
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([userSchema, tweetSchema]),
+})
